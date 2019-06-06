1ad3ec149a858c1f719ae202dbdd17dffc2492de
The Situation <51207584+TheNumber34@users.noreply.github.com>
2019-06-06 13:50:21 -0400

    Created the You class personalized to me.

--- Submisson Assignment/src/You.java
+++ Submisson Assignment/src/You.java
@@ -18,13 +18,37 @@
  *  - Add achiu-frhsd as a collaborator
  *
  */
-public class You{
+public class You
+{
 
 	private String firstName;
 	private String lastName;
 	private String favoriteAnimal;
 	private int age;
 
-	// Write your code here
+	public You()
+	{
+		firstName="Matthew";
+		lastName="Holdorf";
+		favoriteAnimal="Puffin";
+		age=17;
+	}
 	
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public String setFavoriteAnimal(String animal)
+	{
+		favoriteAnimal=animal;
+		return animal;
+	}
+	
+	public int happyBirthday()
+	{
+		System.out.println("Happy Birthday!");
+		age++;
+		return age;
+	}
 }
